460a7ae78bde61697a0de4b1b809475c5355edea
Seb <sebastien@squareup.com>
2025-05-16 15:28:02 -0700

    Initial Commit

---
+++ tim/timApp.swift
@@ -0,0 +1,17 @@
+//
+//  timApp.swift
+//  tim
+//
+//  Created by Sebastien Armand on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct timApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ timTests/timTests.swift
@@ -0,0 +1,16 @@
+//
+//  timTests.swift
+//  timTests
+//
+//  Created by Sebastien Armand on 5/16/25.
+//
+
+import Testing
+
+struct timTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
